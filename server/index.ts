695bdd9a184fbc8e31482aa099fc50cff319b33f
Fabi <fabi.inertia@gmail.com>
2022-06-08 14:49:35 +0200

    generic websocket pubsub

--- server/index.ts
+++ server/index.ts
@@ -3,49 +3,35 @@ import * as ws from "ws";
 
 const PORT = 4000;
 
-const app = express();
+const wssPublish = new ws.Server({ noServer: true });
+const wssSubscribe = new ws.Server({ noServer: true });
 
-const wssClient = new ws.Server({ noServer: true });
-const wssBigScreen = new ws.Server({ noServer: true });
+let subscribers = [];
 
-let globalConnections = [];
-
-wssBigScreen.on("connection", (socket) => {
-  globalConnections.push(socket);
+wssSubscribe.on("connection", (socket) => {
+  subscribers.push(socket);
   socket.on("close", () => {
-    globalConnections.splice(globalConnections.indexOf(socket), 1);
+    subscribers.splice(subscribers.indexOf(socket), 1);
   });
 });
 
-wssClient.on("connection", (socket) => {
+wssPublish.on("connection", (socket) => {
   socket.on("message", (message) => {
-    console.log(message.toString());
-    for (const globalConnection of globalConnections) {
-      globalConnection.send(message);
+    for (const subscriber of subscribers) {
+      subscriber.send(message);
     }
   });
 });
 
-app.get("/bigscreen", (_req, res) => {
-  res.set("Content-Security-Policy", "default-src 'self'");
-  res.send("send me stuff");
-});
-
-app.get("/", (_req, res) => {
-  res.set("Content-Security-Policy", "default-src 'self'");
-  res.send("i iz client");
-});
-
-const server = app.listen(PORT);
-console.log("Listening on " + PORT);
-server.on("upgrade", (request, socket, head) => {
-  if (request.url == "/bigscreen") {
-    wssBigScreen.handleUpgrade(request, socket, head, (socket) => {
-      wssBigScreen.emit("connection", socket, request);
+express().listen(PORT).on("upgrade", (request, socket, head) => {
+  if (request.url == "/sub") {
+    wssSubscribe.handleUpgrade(request, socket, head, (socket) => {
+      wssSubscribe.emit("connection", socket, request);
     });
-  } else if (request.url == "/") {
-    wssClient.handleUpgrade(request, socket, head, (socket) => {
-      wssClient.emit("connection", socket, request);
+  } else if (request.url == "/pub") {
+    wssPublish.handleUpgrade(request, socket, head, (socket) => {
+      wssPublish.emit("connection", socket, request);
     });
   }
 });
+console.log("listening on port " + PORT);
