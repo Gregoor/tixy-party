85f7b4d7677192431f0e9c3d6b86d52bbb2c6dab
Gregor Weber <gregorwbr@gmail.com>
2022-06-09 17:56:00 +0200

    now support my tv for real

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target:
-      "defaults and supports es6-module and supports es6-module-dynamic-import, not opera > 0, not samsung > 0, not and_qq > 0, chrome > 78",
-  },
 });
